343a8f8c595e6bb62c8063db79d5de4656937d4d
justinc04 <didijustin04@gmail.com>
2023-04-20 19:46:25 -0500

    Add more images

--- FPS Game/Assets/_Scripts/ScoreboardItem.cs
+++ FPS Game/Assets/_Scripts/ScoreboardItem.cs
@@ -3,19 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 using Photon.Realtime;
 using TMPro;
+using UnityEngine.UI;
 
 public class ScoreboardItem : MonoBehaviour
 {
     public TMP_Text nameText;
     public TMP_Text scoreText;
+    public Slider scoreSlider;
 
     public void Initialize(Player player)
     {
         nameText.text = player.NickName;
+        scoreSlider.maxValue = GameManager.Instance.winScore;
     }
 
     public void SetScore(int score)
     {
         scoreText.text = score.ToString();
+        scoreSlider.value = score;
     }
 }
